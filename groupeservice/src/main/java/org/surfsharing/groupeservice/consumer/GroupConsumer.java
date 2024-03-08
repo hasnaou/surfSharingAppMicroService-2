a8f87424053fe8ccc408299b73c1f8674be7e65d
Tkarkib Lhassan <73590332+LhassanTkarkib@users.noreply.github.com>
2024-03-08 15:34:12 +0100

    last touches

--- groupeservice/src/main/java/org/surfsharing/groupeservice/consumer/GroupConsumer.java
+++ groupeservice/src/main/java/org/surfsharing/groupeservice/consumer/GroupConsumer.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import org.surfsharing.groupeservice.dto.GroupeDto;
-import org.surfsharing.groupeservice.entity.Groupe;
+
 import org.surfsharing.groupeservice.service.IGroupeService;
 import org.surfsharing.postservice.dto.PostDto;
 
